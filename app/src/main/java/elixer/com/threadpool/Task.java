cc0c7b8681186c4ac78d13660ca2c7f1f84730a9
Shivamdhuria <shivam.dhuria07@gmail.com>
2019-05-27 13:38:01 +0530

    Added documention

--- app/src/main/java/elixer/com/threadpool/Task.java
+++ app/src/main/java/elixer/com/threadpool/Task.java
@@ -16,30 +16,30 @@ public class Task implements Runnable {
     private ProgressBar progressBar;
     private TextView statusTextView;
 
-
-    public Task(Context mContext, String taskName, ProgressBar progressBar, TextView statusTextView) {
+        public Task(Context mContext, String taskName, ProgressBar progressBar, TextView statusTextView) {
         this.taskName = taskName;
         this.mContext = mContext;
         this.progressBar = progressBar;
         this.statusTextView = statusTextView;
     }
-
-    /*
+  /*
     Task to be done in background.
      */
     @Override
     public void run() {
-
-        int step = generateRandomStep();
-
-        // Log.d("Task", String.valueOf(step));
+ int step = generateRandomStep();
         try {
-
-            for (int i = 0; i < 100; ) {
-                i = i + step;
-
-                //Update Ui
+            for (int i = 0; i < 100; )
+            {
+                 i = i + step;
                 final int finalI = i;
+                
+                
+                /*
+                To update the progressBar and status TextView, we need to create a new Runnable that update theam and also runs
+                on the UI thread as currently this is running in a background thread.
+                */
+                
                 ((Activity) mContext).runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -60,7 +60,6 @@ public class Task implements Runnable {
 
     //Generate a Random number that is a factor of 100
     private int generateRandomStep() {
-
         int multiple[] = new int[]{2,5, 10, 20, 25, 50};
         final int random = new Random().nextInt(5);
         return multiple[random];
